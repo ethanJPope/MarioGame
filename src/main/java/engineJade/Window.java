22bad03ed7808d4cf60def1718875c82d956db17
Ethan <ethan.j.pope09@gmail.com>
2024-06-25 18:52:58 -0700

    adding alph blending and z-indexing

--- src/main/java/engineJade/Window.java
+++ src/main/java/engineJade/Window.java
@@ -26,9 +26,9 @@ public class Window {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
-        r = 0.7f;
-        g = 0;
-        b = 0;
+        r = 1;
+        g = 1;
+        b = 1;
         a = 1;
     }
 
@@ -111,6 +111,9 @@ public class Window {
         glfwShowWindow(glfwWindow);
 
         GL.createCapabilities();
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
         Window.changeScene(0);
     }
